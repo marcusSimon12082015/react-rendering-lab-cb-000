e84434a4640c31b83a90cc09d84d489a172c5cdb
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-07 11:01:13 +0000

    Done.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -9,6 +9,11 @@ class Animation extends React.Component {
     };
   }
 
+  componentWillUpdate()
+  {
+    this.showLoadingBar();
+  }
+
   getNewCat = () => {
     fetch("http://api.giphy.com/v1/gifs/random?rating=g&api_key=dc6zaTOxFJmzC")
       .then((res, err) => {
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,6 +11,15 @@ class Rating extends React.Component {
     };
   }
 
+  componentWillReceiveProps(nextProps)
+  {
+    if (nextProps.rating > this.props.rating) {
+      this.setState({increasing:true,decreasing:false});
+    }else if(nextProps.rating < this.props.rating){
+      this.setState({decreasing:true,increasing:false});
+    }
+  }
+
   render() {
     let trend = 'stable';
     if (this.state.increasing) {
