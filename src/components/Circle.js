5d169955d7d4e83277cf3aa314486b563ce621a4
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-07 10:55:18 +0000

    Automatically backed up by Learn

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Circle extends React.Component {
 
   shouldComponentUpdate(nextProps,nextState){
-    return nextProps !== this.props
+    return (nextProps !== this.props)
   }
 
   render() {
